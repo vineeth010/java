aca78018305c742aed1644eb3b22e132e06fcc39
vineeth010 <vineethj010@gmail.com>
2022-08-30 08:28:02 +0000

    first version of Junit

---
+++ src/testpacc/Checking.java
@@ -0,0 +1,7 @@
+package testpacc;
+
+public class Checking {
+public static void main(String[] args) {
+	System.out.println("Hello");
+}
+}
---
+++ src/testpacc/testRun.java
@@ -0,0 +1,9 @@
+package testpacc;
+import org.junit.Before;
+import org.junit.Test;
+
+@Test
+public class testRun {
+	
+
+}
